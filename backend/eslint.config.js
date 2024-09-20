368d5bdf36af2a2ead040e06048bed372463242b
jhordyess <dev@jhordyess.com>
2024-09-19 21:48:32 -0400

    chore: Updated eslint to 9

---
+++ backend/eslint.config.js
@@ -0,0 +1,17 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+/** @type { import("eslint").Linter.Config[] } */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  {
+    languageOptions: {
+      globals: globals.node
+      // ecmaVersion:"latest", // not required as it is the default
+      // sourceType: 'module' // not required as it is the default
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended
+]
