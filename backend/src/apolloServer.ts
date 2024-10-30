47fe6e439fd5f6ee94554338307d46a1d6e7f47d
jhordyess <dev@jhordyess.com>
2024-10-29 22:28:50 -0400

    fix: Add temporary workaround for missing type definition in Apollo Server

--- backend/src/apolloServer.ts
+++ backend/src/apolloServer.ts
@@ -5,9 +5,11 @@ import resolvers from '@/graphql/resolvers'
 import typeDefs from '@/graphql/types'
 
 import { Context } from './types'
+//INFO: Temporary workaround for the missing type definition
+import { type GraphQLResolverMap } from '@apollo/subgraph/dist/schema-helper'
 
 const server = new ApolloServer<Context>({
-  schema: buildSubgraphSchema([{ typeDefs, resolvers }])
+  schema: buildSubgraphSchema([{ typeDefs, resolvers: resolvers as GraphQLResolverMap<Context> }])
 })
 
 await server.start()
